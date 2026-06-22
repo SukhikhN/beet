029ee1dd2a37053e8ddde932f2e225942fadad3b
Mykola Sukhykh <_mk_@ukr.net>
2026-06-23 01:40:29 +0300

    Module 2.3: format source

--- mod2.3-scheduler/src/main.cpp
+++ mod2.3-scheduler/src/main.cpp
@@ -9,17 +9,17 @@ This program uses TaskScheduler to blink three LEDs at different intervals.
 #define _TASK_STD_FUNCTION
 #include <TaskScheduler.h>
 
-struct LEDConfig {
+struct LEDConfig
+{
   uint8_t pin;
   uint32_t interval;
 };
 
 // LED pin and blink period configuration.
 constexpr LEDConfig led_configs[] = {
-  {15, 200},
-  {17, 500},
-  {8, 1000}
-};
+    {15, 200},
+    {17, 500},
+    {8, 1000}};
 
 constexpr uint8_t led_count = sizeof(led_configs) / sizeof(LEDConfig);
 
@@ -31,35 +31,38 @@ Scheduler runner;
 void ledOn(uint8_t index);
 void ledOff(uint8_t index);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   // Configure all LED pins and create periodic blink tasks.
-  for (uint8_t i = 0; i < led_count; i++) {
+  for (uint8_t i = 0; i < led_count; i++)
+  {
     pinMode(led_configs[i].pin, OUTPUT);
 
-    new Task(led_configs[i].interval, TASK_FOREVER, [i]() {
-      ledOn(i);
-    }, &runner, true);
+    new Task(led_configs[i].interval, TASK_FOREVER, [i]()
+             { ledOn(i); }, &runner, true);
   }
 }
 
-void loop() {
+void loop()
+{
   runner.execute();
 }
 
 // Turns one LED on and schedules a one-shot task to turn it off.
-void ledOn(uint8_t index) {
+void ledOn(uint8_t index)
+{
   digitalWrite(led_configs[index].pin, HIGH);
   Serial.printf("LED %d on\n", index);
 
-  new Task(led_on_ms, 1, [index]() {
-    ledOff(index);
-  }, &runner, true);
+  new Task(led_on_ms, 1, [index]()
+           { ledOff(index); }, &runner, true);
 }
 
 // Turns one LED off.
-void ledOff(uint8_t index) {
+void ledOff(uint8_t index)
+{
   digitalWrite(led_configs[index].pin, LOW);
   Serial.printf("LED %d off\n", index);
 }
